a80167d8709e0a259c23c5cafae46e6e3141ecd8
codenotkey <chensenzero@foxmail.com>
2021-09-03 13:39:08 +0800

    deploy

---
+++ precache-manifest.a8b12036fb42964476a8785e14942c62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a82fd2fee1f12f46b4",
+    "url": "/BillProduct/css/app.b9152630.css"
+  },
+  {
+    "revision": "3215392249a4aa57e932",
+    "url": "/BillProduct/css/chunk-vendors.282369e9.css"
+  },
+  {
+    "revision": "3e88f1939d8792e09ff72e4873ef692d",
+    "url": "/BillProduct/img/微信支付.3e88f193.png"
+  },
+  {
+    "revision": "f7f82bc29bd3f939788f1561d8a01f84",
+    "url": "/BillProduct/index.html"
+  },
+  {
+    "revision": "20a82fd2fee1f12f46b4",
+    "url": "/BillProduct/js/app.673855a9.js"
+  },
+  {
+    "revision": "3215392249a4aa57e932",
+    "url": "/BillProduct/js/chunk-vendors.3831f8bc.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/BillProduct/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BillProduct/robots.txt"
+  }
+]);
